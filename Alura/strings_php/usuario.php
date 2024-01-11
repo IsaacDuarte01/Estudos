ae6b5bfde92bdd8d4a9979afcfead8a2e5309bad
Isaac Duarte <152873948+IsaacDuarte01@users.noreply.github.com>
2024-01-11 16:12:36 -0300

    Detalhe
    Caso não foi informado tamanho.

--- Alura/strings_php/usuario.php
+++ Alura/strings_php/usuario.php
@@ -6,3 +6,6 @@ $email = 'isaac.duarte@gmail.com';
 
 //Usamos a Substring que recebe como parametro a DEonde vai buscar, Posição, Tamanho;
 echo substr ($email, 0, 5) . PHP_EOL;
+
+echo substr ($email, 5) . PHP_EOL;
+// Se não determinarmos o tamanho,vai realizar a leitura ate o fim
