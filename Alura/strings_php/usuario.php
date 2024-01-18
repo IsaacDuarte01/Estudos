f67c390d96ac11424d66c7fd714ef1dcdac0be14
Isaac Duarte <152873948+IsaacDuarte01@users.noreply.github.com>
2024-01-18 17:56:15 -0300

    Updateusuario
    Definimos um limite de caracteres pra senha do nosso usuario

--- Alura/strings_php/usuario.php
+++ Alura/strings_php/usuario.php
@@ -3,9 +3,16 @@
 cadastrando e, a partir desse e-mail, você quer criar um nome de usuário*/
 
 $email = 'isaac.duarte@gmail.com';
+$senha = '123';
+
+if (strlen ($senha)< 8) { //Só espera como parametro oque você quer medir
+    echo 'Senha insegura' . PHP_EOL;
+}
+
+$posicaoDoArroba = strpos($email,'@'); //STRPOS recebe como parametro aonde você quer buscar, 'oque');
 
 //Usamos a Substring que recebe como parametro a DEonde vai buscar, Posição, Tamanho;
 echo substr ($email, 0, 5) . PHP_EOL;
 
-echo substr ($email, 5) . PHP_EOL;
+echo substr ($email, $posicaoDoArroba +1) . PHP_EOL;
 // Se não determinarmos o tamanho,vai realizar a leitura ate o fim
