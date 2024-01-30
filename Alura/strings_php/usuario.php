3e452e73c4f0a974a609f9102bee4a1862c9e46f
Isaac Duarte <152873948+IsaacDuarte01@users.noreply.github.com>
2024-01-30 09:14:31 -0300

    list,MB,explode
    *A função explode é utilizada para separar uma string em várias strings diferentes com base em um separador definido.
    *A função list é utilizada para atribuir os valores de um array às variáveis de forma mais prática.

--- Alura/strings_php/usuario.php
+++ Alura/strings_php/usuario.php
@@ -2,6 +2,7 @@
 /*Oque vamos fazer? Imagine que lá no seu sistema você recebe o e-mail de usuário que está se 
 cadastrando e, a partir desse e-mail, você quer criar um nome de usuário*/
 
+$nome = 'Isaac Duarte';
 $email = 'isaac.duarte@gmail.com';
 $senha = 'íííííí';
 
@@ -18,9 +19,13 @@ if (mb_strlen ($senha)< 8) { //Só espera como parametro oque você quer medir
 $posicaoDoArroba = strpos($email,'@'); //STRPOS recebe como parametro aonde você quer buscar, 'oque');
 
 
-$usuario = substr ($email, 0, $posicaoDoArroba);
+$usuario = mb_substr ($email, 0, $posicaoDoArroba);
 //Usamos a Substring que recebe como parametro a DEonde vai buscar, Posição, Tamanho; Se não determinarmos o tamanho,vai realizar a leitura ate o fim
 
 //STRTOLOWER para fazer o inverso (minusculo)
 echo strtoupper ($usuario) . PHP_EOL; //STRTOUPPER não transforma nossa string em maiuscula mas sim nos retorna OUTRA string que tenha o conteudo do $usuario em letra maiuscula
 echo substr ($email, $posicaoDoArroba +1) . PHP_EOL;
+
+list($nome, $sobrenome) = (explode(' ', $nome));// Explode recebe dois parametros, aonde vai acontecer a separação da string no nosso caso é o espaço ' ' e o segundo é oque quer explodir, no nosso caso o $nome
+echo 'Nome:' . $nome . PHP_EOL; 
+echo 'Sobrenome:' . $sobrenome . PHP_EOL;
