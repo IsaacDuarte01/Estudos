c6c9f105fb07fa7a7608b6e5c106c2f1dab4fb30
Isaac Duarte <152873948+IsaacDuarte01@users.noreply.github.com>
2024-04-08 15:33:00 -0300

    heredoc
    indentação > coment

---
+++ Alura/strings_php/email.php
@@ -0,0 +1,24 @@
+<?php       
+
+
+
+function geraEmail(string $nome): void
+{
+/* eu tenho o delimitador chamado de FINAL, tudo que tiver de espaço antes do FINAL,
+ou seja, do início da linha de código até o início do FIM, o delimitador escrito,
+o PHP vai contar esse número de espaços e dentro da minha string, todo o espaço inicial ele vai remover
+,a mesma quantidade de espaços. */
+//HEREDOC O NOME DO <<<
+    $conteudoEmail = <<<FINAL
+    Ola, $nome
+
+    Estamos entrando em ctt para
+    {Motivo do ctt}
+
+    {assinatura}
+    FINAL;
+
+    echo $conteudoEmail . PHP_EOL;
+}
+
+geraEmail('isaac');
