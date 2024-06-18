a47d17ccb447b46e6d968a20892aef91c1c92a88
Isaac Duarte <152873948+IsaacDuarte01@users.noreply.github.com>
2024-06-18 19:30:24 -0300

    Operadores
    Comparação,logico e matematico

--- Alura/aplicação_php/primeiro.php
+++ Alura/aplicação_php/primeiro.php
@@ -1,12 +1,20 @@
 <?php
 // screen_math
 
-echo "Bem vindo(a) ao screen math!
-";
+echo "Bem vindo(a) ao screen math!";
 
 $nomeFilme = "Top gun - maverick" ;
 $anoLancamento = 2022;
-$notaFilme = 8.8;
-$incluidoNoPlano = true;
 
-echo "$nomeFilme";
+$somaDeNotas = 4;
+$somaDeNotas += 1;
+$somaDeNotas += 2; // Operadores junto com o de atribuição ele funciona como acumulativo.
+$somaDeNotas += 7;
+$somaDeNotas += 6;
+
+$notaFilme = $somaDeNotas / 5;
+$planoPrime = true;
+
+$incluidoNoPlano = $PlanoPrime || $anoLancamento < 2020 ;
+
+echo "$notaFilme" . PHP_EOL;
