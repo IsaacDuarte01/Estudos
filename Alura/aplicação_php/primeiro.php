afb0685c89cffcbadcd6c2907b9de037f9413a32
Isaac Duarte <152873948+IsaacDuarte01@users.noreply.github.com>
2024-06-18 18:59:39 -0300

    Primeira
    tipos,exibição,variaveis

---
+++ Alura/aplicação_php/primeiro.php
@@ -0,0 +1,12 @@
+<?php
+// screen_math
+
+echo "Bem vindo(a) ao screen math!
+";
+
+$nomeFilme = "Top gun - maverick" ;
+$anoLancamento = 2022;
+$notaFilme = 8.8;
+$incluidoNoPlano = true;
+
+echo "$nomeFilme";
